a645f1300a4a1decedbdb2ce75cafa7531fadd80
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-24 00:55:07 -0400

    oops

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import { publicProvider } from 'wagmi/providers/public';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const { chains, provider } = configureChains(
   [chain.goerli], // you can add more chains here like chain.mainnet, chain.optimism etc.
@@ -15,7 +14,7 @@ const { chains, provider } = configureChains(
     jsonRpcProvider({
       rpc: () => {
         return {
-          http: 'https://rpc.ankr.com/eth_goerli', // go to https://www.ankr.com/protocol/ to get a free RPC for your network
+          http: "https://rpc.ankr.com/eth_goerli", // go to https://www.ankr.com/protocol/ to get a free RPC for your network
         };
       },
     }),
@@ -24,7 +23,7 @@ const { chains, provider } = configureChains(
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'Next.js Chakra Rainbowkit Wagmi Starter',
+  appName: "Next.js Chakra Rainbowkit Wagmi Starter",
   chains,
 });
 
@@ -34,7 +33,7 @@ const wagmiClient = createClient({
   provider,
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
       <WagmiConfig client={wagmiClient}>
